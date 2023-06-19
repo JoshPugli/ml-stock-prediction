ac221c2123ec89281c37f3a6bc482bd31d04341b
Josh Puglielli <joshpuglielli@pop-os.localdomain>
2023-06-19 14:32:17 -0500

    fixed df sorting

--- pred.py
+++ pred.py
@@ -2,5 +2,14 @@ import numpy as np
 import pandas as pd
 
 # Start with amazon data
-df = pd.read_csv('./data/AMZN_2006-01-01_to_2018-01-01.csv')
+df = pd.read_csv('./data/AMZN_2006-01-01_to_2018-01-01.csv', index_col="Date", parse_dates=True)
+
+# Drop the "name" column
+df.drop("Name", axis=1, inplace=True)
+
+# Sort the data by date
+df.sort_values(by="Date", inplace=True)
+
 print(df.head())
+print(df.tail())
+print(df.shape)
